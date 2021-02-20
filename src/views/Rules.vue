1850c985f2ea940092070cbd4a7c2921c5d8816b
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-20 21:20:19 +0200

    add Rules component

---
+++ src/views/Rules.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rules m-2">
+    <!-- Banner -->
+    <div
+      class="bg-cover bg-dnd-descent bg-center h-auto text-white py-24 px-10 object-fill"
+    >
+      <div class="text-center">
+        <p class="font-bold text-3xl text-black uppercase">Rules</p>
+      </div>
+    </div>
+    <!-- banner -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
