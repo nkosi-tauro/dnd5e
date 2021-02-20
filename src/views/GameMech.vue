69dc43b360016ebce469aee489e04be938bfebe4
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-20 12:37:12 +0200

    added axios

--- src/views/GameMech.vue
+++ src/views/GameMech.vue
@@ -1,21 +1,30 @@
 <template>
-    <div>
-        Game Mechanics
-    </div>
+  <div class="game-mechanics m-2">Game Mechanics {{mech}}</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, onMounted, toRefs } from "vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 export default defineComponent({
-    setup () {
-        
+  setup() {
+    const state = reactive({
+      mech: [],
+    });
 
-        return {}
-    }
-})
+    onMounted(async () => {
+      const { data } = await axios.get(
+        "https://www.dnd5eapi.co/api/conditions"
+      );
+
+      state.mech = data
+    });
+
+    return {...toRefs(state)};
+  },
+});
 </script>
 
 <style scoped>
-
 </style>
