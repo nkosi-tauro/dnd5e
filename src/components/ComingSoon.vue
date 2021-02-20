5e39538fcae6cf803004977a90de530d47fca3a2
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-20 12:04:30 +0200

    added new docs & updated Coming Soon Component

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1 class="m-3 mb-10 text-7xl text-center font-semibold">Coming Soon!</h1>
-        <p class="text-center">Do you want to help out with this project?</p>
+        <h1 class="m-3 mb-10 text-7xl text-center font-bold">Coming Soon!</h1>
+        <p class="text-center text-lg font-semibold">Do you want to help out with this project? <br> Checkout the <a href="https://github.com/nkosi-tauro/dnd5e/blob/master/CONTRIBUTING.md" class="text-blue-800 italic" target="_blank">Contributing.md</a> for more infomation.</p>
     </div>
 </template>
 
