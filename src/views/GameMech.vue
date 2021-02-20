0b454b56956aa72e5f1dc7e42bcef5828852fe3c
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-20 13:06:32 +0200

    new data table

--- src/views/GameMech.vue
+++ src/views/GameMech.vue
@@ -1,5 +1,95 @@
 <template>
-  <div class="game-mechanics m-2">Game Mechanics {{mech}}</div>
+  <div class="game-mechanics m-2">
+    Game Mechanics
+    <table class="border-collapse w-full">
+      <thead>
+        <tr>
+          <th
+            class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell"
+          >
+            Game Mechanics
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+        >
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Company name</span
+            >
+            Conditions
+          </td>
+
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Status</span
+            >
+            <span class="rounded bg-red-400 py-1 px-3 text-xs font-bold"
+              >{{conditions}}</span
+            >
+          </td>
+
+        </tr>
+        <tr
+          class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+        >
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Company name</span
+            >
+            Damage Types
+          </td>
+
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Status</span
+            >
+            <span class="rounded bg-green-400 py-1 px-3 text-xs font-bold"
+              >active</span
+            >
+          </td>
+        </tr>
+        <tr
+          class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0"
+        >
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Company name</span
+            >
+            Magic Schools
+          </td>
+          <td
+            class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static"
+          >
+            <span
+              class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"
+              >Status</span
+            >
+            <span class="rounded bg-yellow-400 py-1 px-3 text-xs font-bold"
+              >inactive</span
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,7 +100,7 @@ import axios from "axios";
 export default defineComponent({
   setup() {
     const state = reactive({
-      mech: [],
+      conditions: [],
     });
 
     onMounted(async () => {
@@ -18,10 +108,10 @@ export default defineComponent({
         "https://www.dnd5eapi.co/api/conditions"
       );
 
-      state.mech = data
+      state.conditions = data;
     });
 
-    return {...toRefs(state)};
+    return { ...toRefs(state) };
   },
 });
 </script>
