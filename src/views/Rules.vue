bc7745e7b8c183adeb2b36ceecbc34ab241f6239
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-20 22:26:20 +0200

    add rules component & new header banner

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -2,7 +2,7 @@
   <div class="rules m-2">
     <!-- Banner -->
     <div
-      class="bg-cover bg-dnd-descent bg-center h-auto text-white py-24 px-10 object-fill"
+      class="bg-cover bg-dnd-dragon lg:bg-center sm:bg-top  h-auto text-white py-24 px-10 object-fill"
     >
       <div class="text-center">
         <p class="font-bold text-3xl text-black uppercase">Rules</p>
