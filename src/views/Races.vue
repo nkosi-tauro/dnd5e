6919ce202f705cbf36b02311a1de79efdbe32b27
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-21 18:50:26 +0200

    updated race navigation with Race data

--- src/views/Races.vue
+++ src/views/Races.vue
@@ -33,13 +33,12 @@
               <div
                 class="text-sm lg:flex-grow mt-2 animated jackinthebox xl:mx-8"
               >
-                <a
-                  href="#home"
+                <a v-for="{index, name} in races" :key="index"
+                  @click="showRace(index)"
                   class="block lg:inline-block text-md font-bold text-orange-500 sm:hover:border-indigo-400 hover:text-orange-500 mx-2 focus:text-blue-500 p-1 hover:bg-gray-300 sm:hover:bg-transparent rounded-lg"
                 >
-                  HOME
+                  {{name}}
                 </a>
-        
               </div>
             </div>
           </nav>
@@ -51,15 +50,23 @@
 
 <script lang="ts">
 
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, onMounted, toRefs } from "vue";
 import axios from 'axios'
 
 export default defineComponent({
   setup() {
     const state = reactive({
+        races : Array
 
     })
-    return {};
+
+    onMounted(async() => {
+        const {data} = await axios.get("https://www.dnd5eapi.co/api/races")
+        state.races = data.results
+        console.log(state.races)
+        
+    })
+    return {...toRefs(state)};
   },
 });
 </script>
