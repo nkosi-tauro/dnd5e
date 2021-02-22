395fbdd0fe39e0939dfd8a6686c733d2cfcf6960
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-22 13:24:45 +0200

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,12 +38,6 @@
                   to="/"
                   class="block lg:inline-block text-md font-bold text-gray-900 sm:hover:border-indigo-400 hover:text-orange-500 mx-2 focus:text-red-500 p-1 hover:bg-gray-300 sm:hover:bg-transparent rounded-lg"
                 >
-                  BACKGROUNDS
-                </router-link>
-                <router-link
-                  to="/"
-                  class="block lg:inline-block text-md font-bold text-gray-900 sm:hover:border-indigo-400 hover:text-orange-500 mx-2 focus:text-red-500 p-1 hover:bg-gray-300 sm:hover:bg-transparent rounded-lg"
-                >
                   CLASSES
                 </router-link>
                 <router-link
@@ -57,6 +51,12 @@
                   class="block lg:inline-block text-md font-bold text-gray-900 sm:hover:border-indigo-400 hover:text-orange-500 mx-2 focus:text-red-500 p-1 hover:bg-gray-300 sm:hover:bg-transparent rounded-lg"
                 >
                   EQUIPMENT
+                </router-link>
+                <router-link
+                  to="/"
+                  class="block lg:inline-block text-md font-bold text-gray-900 sm:hover:border-indigo-400 hover:text-orange-500 mx-2 focus:text-red-500 p-1 hover:bg-gray-300 sm:hover:bg-transparent rounded-lg"
+                >
+                  MONSTERS
                 </router-link>
                 <router-link
                   to="/mechanics"
