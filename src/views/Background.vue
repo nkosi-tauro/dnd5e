6c3f585c13f0008e424ba85e10ac4165ef9805f1
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-24 14:29:09 +0200

    added background component

---
+++ src/views/Background.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="backgrounds m-2">
+    <!-- Banner -->
+    <Banner
+      :backgroundCover="'bg-cover bg-dnd-cube lg:bg-center sm:bg-top h-auto text-white py-24 px-10 object-fill'"
+    >
+      <div>Backgrounds</div>
+    </Banner>
+    <!-- Banner End -->
+  </div>
+</template>
+
+<script lang="ts">
+import Banner from "../components/Banner.vue";
+import Nav from "../components/Nav.vue";
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  components:{Banner, Nav},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
