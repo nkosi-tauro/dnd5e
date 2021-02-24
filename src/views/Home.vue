b469cc965e5a88f4c572e3e6106f8dcb28f141c6
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-25 01:59:07 +0200

    updated api link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
    <div class="mt-5 w-auto max-h-full text-black py-24 px-10  " >
        <div class="sm:text-center">
         <p class="text-4xl font-bold">Dungeons & Dragons 5th Edition Wiki</p>
-        <p class="text-2xl mb-10 leading-none">A wiki built with <a href="http://http://www.dnd5eapi.co/" target="_blank" rel="noopener noreferrer" class="underline">dnd5eapi</a>...</p>
+        <p class="text-2xl mb-10 leading-none">A wiki built with <a href="https://www.dnd5eapi.co/" target="_blank" rel="noopener noreferrer" class="underline">dnd5eapi</a>...</p>
         <router-link to="/classes"><a  class="bg-black py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">Get Started</a></router-link>
         </div>  
     </div>
