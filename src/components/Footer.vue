ec405c82770e6a67aaba444321ff4ddf5793d83b
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-25 01:50:20 +0200

    updated Home & Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,9 @@
 <template>
-  <div
-    class="w-full bottom-0 mt-20 shadow-md py-6 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800"
-  >
-    <div
-      class="flex flex-col max-w-screen-xl  md:items-center md:justify-between md:flex-row md:px-6 "
-    >
-      <div class=" flex items-center justify-between">
-        <a
-          class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
-          >DnD 5th Edition Wiki 
-          <p class="text-sm">THIS IS A FAN SITE AND IS IN NO WAY ENDORSED BY WIZARDS OF THE COAST</p>
-          <p class="text-sm">BUILT WITH http://www.dnd5eapi.co/</p>
-        </a
-        >
-      </div>
-      <br>
-      
-      
-      <footer
-        class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row"
-      >
-        <div class="px-2 py-2 bg-black rounded-md shadow dark-mode:bg-gray-800">
-          <a href="https://github.com/nkosi-tauro/" target="_blank" rel="noopener noreferrer" class="shadow-md text-white">GitHub</a>
-        </div>
-      </footer>
-    </div>
-  </div>
+    <footer class="w-full bottom-10 sm:text-center  shadow-sm border-t border-grey p-4 pin-b ">
+        <p class="font-bold font-sans text-black">DnD 5th Edition Wiki </p>
+        <p class="font-bold text-sm font-sans text-black">This is a fan site and is in no way endorsed by Wizards of the Coast.</p>
+        <p class="font-bold text-sm font-sans text-black"><a href="https://github.com/nkosi-tauro/" target="_blank" rel="noopener noreferrer" class="underline">GitHub</a></p>
+    </footer>
 </template>
 
 <script>
